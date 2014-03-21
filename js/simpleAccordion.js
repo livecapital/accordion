57e40d146a67c3a27a3e78797127aa2c4a0cad77
Gustavo Hingel Morada <gusmorada@gmail.com>
2014-03-20 22:46:30 -0300

    Prepare plugin to allow extra options and chainable iterable objects

--- js/simpleAccordion.js
+++ js/simpleAccordion.js
@@ -1,7 +1,9 @@
 (function ( $ ) {
-    $.fn.simpleAccordion = function(opts){
-        var options = {}
-        var acc = $(this);
-        return this;
+    $.fn.simpleAccordion = function(options){
+        var settings = $.extend({
+        }, options );
+
+        return this.each(function() {
+        });
     }
 }( jQuery ));
