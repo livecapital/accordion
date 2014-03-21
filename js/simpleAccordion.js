1024e19e24293296769d7885a5cf26eaafdbb123
Gustavo Hingel Morada <gusmorada@gmail.com>
2014-03-21 18:27:52 -0300

    Implementa funções básicas do accordion

--- js/simpleAccordion.js
+++ js/simpleAccordion.js
@@ -1,9 +1,20 @@
 (function ( $ ) {
     $.fn.simpleAccordion = function(options){
         var settings = $.extend({
+            expanseAll: false,
+            collapseAll: false
         }, options );
 
-        return this.each(function() {
+        return this.each(function(ind, obj) {
+            var acc = $(obj);
+            var titles = acc.children('._title');
+            titles.each(function(i, e){
+                var title = $(e)
+                var content = title.next('._content');
+                title.click(function(){
+                    content.toggle();
+                });
+            })
         });
     }
 }( jQuery ));
